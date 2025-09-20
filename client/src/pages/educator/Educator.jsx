6309ee256e35f2eb044e9dfb833d37178be374b0
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-20 20:26:21 +0530

    Refactor Educator component layout to include Navbar and SideBar for improved structure and navigation

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
+import SideBar from "../../components/educator/SideBar";
 
 const Educator = () => {
   return (
-    <div>
-        <h1>Educator</h1>
-        <div>
-            {<Outlet/>}
-        </div>
+    <div className="text-default min-h-screen bg-white">
+      <Navbar />
+      <div className="flex">
+        <SideBar />
+        <div className="flex-1">{<Outlet />}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Educator
+export default Educator;
