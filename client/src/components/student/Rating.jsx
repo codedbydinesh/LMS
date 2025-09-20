839254682fe9b437a656ed97230950e33f1c347c
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-20 08:21:28 +0530

    Enhance Rating component to support dynamic rating and user interaction

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRation, onRate}) => {
+
+  const [rating, setRating] = useState(initialRation || 0)
+
+  const handleRating = (value) => {
+    setRating(value)
+    if(onRate) onRate(value)
+  }
+
+  useEffect(()=>{
+    if(initialRation){
+      setRating(initialRation)
+    }
+  },[initialRation])
+
   return (
-    <div>Rating</div>
+    <div>
+      {
+        Array.from({length: 5}, (_, index)=> {
+          const starValue = index + 1;
+          return (
+            <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500': 'text-gray-400'}`} onClick={() => handleRating(starValue)}>
+              &#9733;
+            </span>
+          )
+        })
+      }
+    </div>
   )
 }
 
